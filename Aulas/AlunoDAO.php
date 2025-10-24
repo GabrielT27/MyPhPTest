2b7c7012f42cb7c3b7acb9c1a728ea879b45c952
GabrielT27 <gabrielmarquesterr@gmail.com>
2025-10-24 18:04:15 -0300

    DAO  de arquivos.

--- Aulas/AlunoDAO.php
+++ Aulas/AlunoDAO.php
@@ -4,6 +4,42 @@ Class AlunoDAO {   // "DAO --> "Data Acess Object"
 
     private $alunos = [];  // Array para armazenamento temporário dos objetos e seus atributos, antes de mandar para o banco de dados. Foi criado incialmente vazio [];
 
+    private $arquivo = "alunos.json"; // Cria o arquivo de json para que os dados sejam armazenados
+
+
+    // Construtor alunoDAO --> carrega os dados do arquivo ao iniciar a aplicação 
+
+    public function __construct() {
+        if (file_exists($this->arquivo)){
+            // Lê o conteúdo do arquivo caso ele já existe
+            $conteudo = file_get_contents
+            ($this->arquivo); // Atribui as informações do arquivo existente á variavel $conteudo
+
+
+            $dados = json_decode( $conteudo, true); // Converte um JSON em array associativo 
+
+            if ($dados) {
+                foreach ($dados as $id => $info) {
+                    $this->alunos[$id] = new Aluno(
+                        $info['id'],
+                        $info ['nome'],
+                        $info['curso']
+                    );
+                }
+            }
+        }
+    }
+
+
+    // Método Auxiliar -> salva o array de alunos no arquivo 
+
+    private function salvarEmArquivo() {
+        $dados = [];
+    }
+
+    
+
+
     public function criarAluno(Aluno $aluno) // Método CREATE --> para criar um novo objeto
     {
         $this->alunos[$aluno->getId()]=$aluno;
